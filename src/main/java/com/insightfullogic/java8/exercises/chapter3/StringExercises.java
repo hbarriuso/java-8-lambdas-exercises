cf5f40070cc10e4d806e95c316a24b265a6fcd65
hbarriuso <hbarriuso@gmail.com>
2018-04-24 23:39:38 +0200

    String exercises implementation

--- src/main/java/com/insightfullogic/java8/exercises/chapter3/StringExercises.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter3/StringExercises.java
@@ -1,21 +1,20 @@
 package com.insightfullogic.java8.exercises.chapter3;
 
-import com.insightfullogic.java8.exercises.Exercises;
-
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+
+import static java.util.Comparator.comparing;
 
 public class StringExercises {
 
     // Question 7
     public static int countLowercaseLetters(String string) {
-        return Exercises.replaceThisWithSolution();
+        return (int) string.chars().filter(Character::isLowerCase).count();
     }
 
     // Question 8
     public static Optional<String> mostLowercaseString(List<String> strings) {
-        return Exercises.replaceThisWithSolution();
+        return strings.stream().max(comparing(StringExercises::countLowercaseLetters));
     }
 
 }
