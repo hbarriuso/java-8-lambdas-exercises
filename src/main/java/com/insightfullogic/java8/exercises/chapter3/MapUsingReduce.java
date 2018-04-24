ad83338a493f4fb25e19951699892ea19f1b5d4f
hbarriuso <hbarriuso@gmail.com>
2018-04-25 00:08:29 +0200

    MapUsingReduce exercise implementation

--- src/main/java/com/insightfullogic/java8/exercises/chapter3/MapUsingReduce.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter3/MapUsingReduce.java
@@ -1,7 +1,5 @@
 package com.insightfullogic.java8.exercises.chapter3;
 
-import com.insightfullogic.java8.exercises.Exercises;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
@@ -13,7 +11,16 @@ import java.util.stream.Stream;
 public class MapUsingReduce {
 
     public static <I, O> List<O> map(Stream<I> stream, Function<I, O> mapper) {
-        return Exercises.replaceThisWithSolution();
+        return stream.reduce(new ArrayList<>(),
+                (os, i) -> {
+                    ArrayList<O> result = new ArrayList<>(os);
+                    result.add(mapper.apply(i));
+                    return result;
+                }, (os, os2) -> {
+                    ArrayList<O> result = new ArrayList<>(os);
+                    result.addAll(os2);
+                    return result;
+                });
     }
 
 }
