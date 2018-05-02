c42ce3c11597e8a018803bd25ecb51eca847347e
hbarriuso <hbarriuso@gmail.com>
2018-05-02 22:52:24 +0200

    LongestName exercise of chapter 5 implementation

--- src/main/java/com/insightfullogic/java8/exercises/chapter5/LongestName.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter5/LongestName.java
@@ -1,23 +1,23 @@
 package com.insightfullogic.java8.exercises.chapter5;
 
 import com.insightfullogic.java8.examples.chapter1.Artist;
-import com.insightfullogic.java8.exercises.Exercises;
 
-import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static java.util.Comparator.comparing;
 
 public class LongestName {
+    private static final Function<Artist, Integer> getNameLength = artist -> artist.getName().length();
 
     public static Artist byReduce(List<Artist> artists) {
-        return Exercises.replaceThisWithSolution();
+        return artists.stream().reduce(BinaryOperator.maxBy(comparing(getNameLength))).get();
     }
 
     public static Artist byCollecting(List<Artist> artists) {
-        return Exercises.replaceThisWithSolution();
+        return artists.stream().collect(Collectors.maxBy(comparing(getNameLength))).get();
     }
 
 }
