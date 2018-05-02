5968cb0dfc17da2b015294ae7317c152e19af6d1
hbarriuso <hbarriuso@gmail.com>
2018-05-02 23:13:43 +0200

    GroupingBy exercise of chapter 5 implementation

--- src/main/java/com/insightfullogic/java8/exercises/chapter5/GroupingBy.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter5/GroupingBy.java
@@ -1,7 +1,5 @@
 package com.insightfullogic.java8.exercises.chapter5;
 
-import com.insightfullogic.java8.exercises.Exercises;
-
 import java.util.*;
 import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
@@ -19,27 +17,37 @@ public class GroupingBy<T, K> implements Collector<T, Map<K, List<T>>, Map<K, Li
 
     @Override
     public Supplier<Map<K, List<T>>> supplier() {
-        return Exercises.replaceThisWithSolution();
+        return HashMap::new;
     }
 
     @Override
     public BiConsumer<Map<K, List<T>>, T> accumulator() {
-        return Exercises.replaceThisWithSolution();
+        return (map, t) -> {
+            K key = classifier.apply(t);
+            List<T> elements = map.computeIfAbsent(key, k -> new ArrayList<>());
+            elements.add(t);
+        };
     }
 
     @Override
     public BinaryOperator<Map<K, List<T>>> combiner() {
-        return Exercises.replaceThisWithSolution();
+        return (left, right) -> {
+            right.forEach((key, value) -> left.merge(key, value, (ts, ts2) -> {
+                ts.addAll(ts2);
+                return ts;
+            }));
+            return left;
+        };
     }
 
     @Override
     public Function<Map<K, List<T>>, Map<K, List<T>>> finisher() {
-        return Exercises.replaceThisWithSolution();
+        return Function.identity();
     }
 
     @Override
     public Set<Characteristics> characteristics() {
-        return Exercises.replaceThisWithSolution();
+        return Collections.singleton(Characteristics.IDENTITY_FINISH);
     }
 
 }
