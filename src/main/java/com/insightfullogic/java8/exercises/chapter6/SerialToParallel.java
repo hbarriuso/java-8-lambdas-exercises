55df72949ee48ce60c81930128559275ae4c578a
hbarriuso <hbarriuso@gmail.com>
2018-05-06 21:21:27 +0200

    Removed unused import

--- src/main/java/com/insightfullogic/java8/exercises/chapter6/SerialToParallel.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter6/SerialToParallel.java
@@ -1,7 +1,5 @@
 package com.insightfullogic.java8.exercises.chapter6;
 
-import com.insightfullogic.java8.exercises.Exercises;
-
 import java.util.stream.IntStream;
 
 public class SerialToParallel {
